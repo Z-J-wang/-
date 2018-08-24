6b552658c3f808295aa28f2a3b504ff60816c189
jie <1356573671@qq.com>
2018-08-24 08:33:34 +0800

    jie 2/24 早上

--- xinlang/js/xinlang.js
+++ xinlang/js/xinlang.js
@@ -6,3 +6,11 @@ function _hidden(obj){
 	var elem = obj.firstChild.nextSibling.nextSibling.nextSibling;
 	elem.style.display = "none";
 }
+function _show_1(obj){
+	var elem = obj.nextSibling.nextSibling;
+	elem.style.display = "inherit";
+}
+function _hidden_1(obj){
+	var elem = obj.nextSibling.nextSibling;
+	elem.style.display = "none";
+}
