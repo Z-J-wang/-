fb93285a873eebf6942cdcde8e189aa51d5ce273
jie <1356573671@qq.com>
2018-08-24 12:18:41 +0800

    jie

--- xinlang/js/xinlang.js
+++ xinlang/js/xinlang.js
@@ -1,3 +1,4 @@
+
 function _show(obj){
 	var elem = obj.firstChild.nextSibling.nextSibling.nextSibling;
 	elem.style.display = "inherit";
@@ -13,4 +14,20 @@ function _show_1(obj){
 function _hidden_1(obj){
 	var elem = obj.nextSibling.nextSibling;
 	elem.style.display = "none";
+}
+function search_classify(){
+	var elem = document.querySelector(".search_classify");
+	if(elem.style.display == 'inherit'){
+		elem.style.display = 'none';
+	} else{
+		elem.style.display = 'inherit';
+	}
+}
+function search_reco(){
+	var elem = document.querySelector(".search_reco");
+	if(elem.style.display == 'inherit'){
+		elem.style.display = 'none';
+	} else{
+		elem.style.display = 'inherit';
+	}
 }
