82bf7501e2d19cd485cd376e4972f02a9e5eb5b7
jie <1356573671@qq.com>
2018-09-01 10:54:15 +0800

    9/1 jie

--- Gluttonous_Snake/js/snake.js
+++ Gluttonous_Snake/js/snake.js
@@ -1,42 +1,61 @@
 $(document).ready(function(){
-	var $span = 20;
-	var $snakes = $(".background>div");
-	var $snake_head = $(".snake");
-	var $H_Top = $snake_head.css("top");
-	var $H_Left = $snake_head.css("left");
-	var coordinate_x = new Array();
-	var coordinate_y = new Array();
-	for(var i=0;i < $snakes.length;i++){
-		coordinate_x[i] = $snakes.eq(i).css("top");
-		coordinate_y[i] = $snakes.eq(i).css("left");
-	}
-	
-	
+//	var $span = 20;
+//	var $snakes = $(".background>div");
+//	var $snake_head = $(".snake");
+//	var $H_Top = $snake_head.css("top");
+//	var $H_Left = $snake_head.css("left");
+//	var coordinate_x = new Array();
+//	var coordinate_y = new Array();
+//	for(var i=0;i < $snakes.length;i++){
+//		coordinate_x[i] = $snakes.eq(i).css("top");
+//		coordinate_y[i] = $snakes.eq(i).css("left");
+//	}
+	Crate_start_snake_boby();
+	Crate_start_snake_boby();
+	create_foot();
 	$(window).keydown(function(event){
 		switch (event.keyCode){
-			case 38:move_top();
+			case 38:{
+				move_top();
+				eat();
+			}
 				break;
-			case 40:move_bottom();
+			case 40:{
+				move_bottom();
+				eat();
+			}
 				break;
-			case 37:move_left();
+			case 37:{
+				move_left();
+				eat();
+			}
 				break;
-			case 39:move_right();
+			case 39:{
+				move_right();
+				eat();
+			}
 				break;
 		}
 	})
 	
+
 	$("#btn").click(function(){
-		Crate_snake_boby();
+		Crate_start_snake_boby();
+//		create_foot();
+		
 	})
 	
 	
 })
 
-
+var $background = $('.background');
+var coordinate_x = new Array();
+var coordinate_y = new Array();
+var status = "Right";
 
 //创建一节蛇身
-function Crate_snake_boby(){
-	var $background = $('.background');
+function Crate_start_snake_boby(){
+//	var $background = $('.background');
 	var $newSnake_boby = $("<div class='snake_boby'></div>");
 	var $snakes = $(".background>div");
 	var L_left = parseInt($snakes.eq($snakes.length-1).css("left"))-20+'px';
@@ -44,16 +63,63 @@ function Crate_snake_boby(){
 	$newSnake_boby.css("left",L_left);
 	$newSnake_boby.css("top",L_Top);
 	$background.append($newSnake_boby);
-	
 }
 
+function Crate_snake_boby(){
+var $newSnake_boby = $("<div class='snake_boby'></div>");
+	var $snakes = $(".background>div");
+	var Last_boby_x = parseInt($snakes.eq($snakes.length-1).css("left"));
+	var LastButOne_boby_x = parseInt($snakes.eq($snakes.length-2).css("left"));
+	var Last_boby_y = parseInt($snakes.eq($snakes.length-1).css("top"));
+	var LastButOne_boby_y = parseInt($snakes.eq($snakes.length-2).css("top"));
+	console.log(LastButOne_boby_x,LastButOne_boby_x)
+	if(Last_boby_x == LastButOne_boby_x){
+		if(Last_boby_y > LastButOne_boby_y){
+			$newSnake_boby.css("left",Last_boby_x);
+			$newSnake_boby.css("top",Last_boby_y+20+'px');
+		}else if(Last_boby_y < LastButOne_boby_y){
+			$newSnake_boby.css("left",Last_boby_x);
+			$newSnake_boby.css("top",Last_boby_y-20+'px');
+		}
+	}else if(Last_boby_y == LastButOne_boby_y){
+		if(Last_boby_x > LastButOne_boby_x){
+			$newSnake_boby.css("left",Last_boby_x+20+'px');
+			$newSnake_boby.css("top", Last_boby_y);
+		}else if(Last_boby_x < LastButOne_boby_x){
+			$newSnake_boby.css("left",Last_boby_x-20+'px');
+			$newSnake_boby.css("top", Last_boby_y);
+		}
+	}
+	$background.append($newSnake_boby);
+	console.log($newSnake_boby.css("left"),$newSnake_boby.css("top"))
+}
+
+function create_foot(){
+	
+	this.foot_x = Math.floor(Math.random()*50)*20;
+	this.foot_y = Math.floor(Math.random()*30)*20;
+	var $foot = $("<span class='foot'></span>");
+	$foot.css("top",foot_y+"px");
+	$foot.css("left",foot_x+"px");
+	$background.append($foot);
+	console.log(foot_x,foot_y);
+}
+
+function eat(){
+	var $snakes = $(".background>div");
+	var $H_Top1 = parseInt($snakes.eq(0).css("top"));;
+	var $H_Left1 = parseInt($snakes.eq(0).css("left"));
+	if($H_Top1 == foot_y && $H_Left1 == foot_x){
+		$(".foot").remove();
+		Crate_snake_boby();
+		create_foot();
+	}
+}
 
 function move_right(){
 	var $snakes = $(".background>div");
 	var $H_Top = parseInt($snakes.eq(0).css("top"));
 	var $H_Left = parseInt($snakes.eq(0).css("left"));
-	var coordinate_x = new Array();
-	var coordinate_y = new Array();
 	for(var i=0;i < $snakes.length;i++){
 		coordinate_y[i] = $snakes.eq(i).css("top");
 		coordinate_x[i] = $snakes.eq(i).css("left");
@@ -64,16 +130,7 @@ function move_right(){
 			$snakes.eq(i).css("top",coordinate_y[i-1]);
 			$snakes.eq(i).css("left",coordinate_x[i-1]);
 		}
-		
-		var $H_Top1 = parseInt($snakes.eq(0).css("top"));;
-		var $H_Left1 = parseInt($snakes.eq(0).css("left"));
-		console.log($H_Left1,$H_Top1)
-		for(var j = 0;j < $snakes.length;j++){
-			console.log(parseInt(coordinate_x[j]),parseInt(coordinate_y[j]));
-			if($H_Top1 == parseInt(coordinate_y[j]) && $H_Left1 == parseInt(coordinate_x[j])){
-				alert("Game Over");
-			}
-		}
+		collide_boby();
 	}else{
 		return false;
 	}
@@ -83,8 +140,6 @@ function move_left(){
 	var $snakes = $(".background>div");
 	var $H_Top = parseInt($snakes.eq(0).css("top"));
 	var $H_Left = parseInt($snakes.eq(0).css("left"));
-	var coordinate_x = new Array();
-	var coordinate_y = new Array();
 	for(var i=0;i < $snakes.length;i++){
 		coordinate_y[i] = $snakes.eq(i).css("top");
 		coordinate_x[i] = $snakes.eq(i).css("left");
@@ -95,15 +150,7 @@ function move_left(){
 			$snakes.eq(i).css("top",coordinate_y[i-1]);
 			$snakes.eq(i).css("left",coordinate_x[i-1]);
 		}
-		var $H_Top1 = parseInt($snakes.eq(0).css("top"));;
-		var $H_Left1 = parseInt($snakes.eq(0).css("left"));
-		console.log($H_Left1,$H_Top1)
-		for(var j = 0;j < $snakes.length;j++){
-			console.log(parseInt(coordinate_x[j]),parseInt(coordinate_y[j]));
-			if($H_Top1 == parseInt(coordinate_y[j]) && $H_Left1 == parseInt(coordinate_x[j])){
-				alert("Game Over");
-			}
-		}
+	collide_boby();
 	}else{
 		return false;
 	}
@@ -112,8 +159,6 @@ function move_top(){
 	var $snakes = $(".background>div");
 	var $H_Top = parseInt($snakes.eq(0).css("top"));
 	var $H_Left = parseInt($snakes.eq(0).css("left"));
-	var coordinate_x = new Array();
-	var coordinate_y = new Array();
 	for(var i=0;i < $snakes.length;i++){
 		coordinate_y[i] = $snakes.eq(i).css("top");
 		coordinate_x[i] = $snakes.eq(i).css("left");
@@ -124,15 +169,7 @@ function move_top(){
 			$snakes.eq(i).css("top",coordinate_y[i-1]);
 			$snakes.eq(i).css("left",coordinate_x[i-1]);
 		}
-		var $H_Top1 = parseInt($snakes.eq(0).css("top"));;
-		var $H_Left1 = parseInt($snakes.eq(0).css("left"));
-		console.log($H_Left1,$H_Top1)
-		for(var j = 0;j < $snakes.length;j++){
-			console.log(parseInt(coordinate_x[j]),parseInt(coordinate_y[j]));
-			if($H_Top1 == parseInt(coordinate_y[j]) && $H_Left1 == parseInt(coordinate_x[j])){
-				alert("Game Over");
-			}
-		}
+	collide_boby();
 	}else{
 		return false;
 	}
@@ -141,8 +178,6 @@ function move_bottom(){
 	var $snakes = $(".background>div");
 	var $H_Top = parseInt($snakes.eq(0).css("top"));
 	var $H_Left = parseInt($snakes.eq(0).css("left"));
-	var coordinate_x = new Array();
-	var coordinate_y = new Array();
 	for(var i=0;i < $snakes.length;i++){
 		coordinate_y[i] = $snakes.eq(i).css("top");
 		coordinate_x[i] = $snakes.eq(i).css("left");
@@ -153,16 +188,25 @@ function move_bottom(){
 			$snakes.eq(i).css("top",coordinate_y[i-1]);
 			$snakes.eq(i).css("left",coordinate_x[i-1]);
 		}
-		var $H_Top1 = parseInt($snakes.eq(0).css("top"));;
-		var $H_Left1 = parseInt($snakes.eq(0).css("left"));
-		console.log($H_Left1,$H_Top1)
-		for(var j = 0;j < $snakes.length;j++){
-			console.log(parseInt(coordinate_x[j]),parseInt(coordinate_y[j]));
-			if($H_Top1 == parseInt(coordinate_y[j]) && $H_Left1 == parseInt(coordinate_x[j])){
-				alert("Game Over");
-			}
-		}
+collide_boby();
 	}else{
 		return false;
 	}
+}
+function collide_boby(){
+		var $snakes = $(".background>div");
+		var $H_Top1 = parseInt($snakes.eq(0).css("top"));;
+		var $H_Left1 = parseInt($snakes.eq(0).css("left"));
+//		console.log($H_Left1,$H_Top1)
+		for(var i=0;i < $snakes.length;i++){
+			coordinate_y[i] = $snakes.eq(i).css("top");
+			coordinate_x[i] = $snakes.eq(i).css("left");
+		}
+		for(var j = 1;j < $snakes.length;j++){
+//			console.log(parseInt(coordinate_x[j]),parseInt(coordinate_y[j]));
+			if($H_Top1 == parseInt(coordinate_y[j]) && $H_Left1 == parseInt(coordinate_x[j])){
+				alert("Game Over");
+				return false;
+			}
+		}
 }
