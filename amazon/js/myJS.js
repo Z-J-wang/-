23f51be04cacac0b760248791dfdc4656ec39e40
jie <1356573671@qq.com>
2018-09-19 20:44:48 +0800

    amazon 首页基本完成

--- amazon/js/myJS.js
+++ amazon/js/myJS.js
@@ -52,4 +52,24 @@ $(document).ready(function(){
 	
 	//倒计时
 	downTimeFun();
+	
+	//长条轮播
+	$("#left").click(function(){
+		carousel_srcoll_left(".carousel_long_content",1480,2,1000);
+	})
+	$("#right").click(function(){
+		carousel_scroll_right(".carousel_long_content",1480,2,1000);
+	})
+	
+	$("#left_2").click(function(){
+		carousel_srcoll_left(".carousel_long_content_2>",1480,2,1000);
+	})
+	$("#right_2").click(function(){
+		carousel_scroll_right(".carousel_long_content_2",1480,2,1000);
+	})
+	
+	//返回顶部
+	$("#return_top").click(function(){
+		$('body,html').animate({scrollTop:0},500);
+	})	
 })
