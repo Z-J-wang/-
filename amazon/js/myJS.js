3afc7fd5f1c9a17ca5f6a1b5dad3128b9bc66e7f
jie <1356573671@qq.com>
2018-09-19 13:28:27 +0800

    amazon项目 9/19 noon

--- amazon/js/myJS.js
+++ amazon/js/myJS.js
@@ -1,8 +1,8 @@
-//$(function () { $("[data-toggle='popover']").popover(); });
-
+$(function () { $("[data-toggle='popover']").popover(); });
+//$(function () { $(".popover-options").popover({html : true });});
 $(document).ready(function(){
 	
-	$(".a_p>a").hover(function(){
+	$(".a_p>a,.prime_info").hover(function(){
 		$(".prime_info").show();
 	},function(){
 		$(".prime_info").hide()
@@ -20,5 +20,36 @@ $(document).ready(function(){
 //		$(".dropdown_menu_2").hide()
 //	})
 	
+	$(".user_bar,.user_interface").hover(function(){
+		$(".user_interface").show();
+		$(".bg_color").show();
+	},function(){
+		$(".user_interface").hide();
+		$(".bg_color").hide();
+	})
 	
+	$(".try_prime,.prime_way").hover(function(){
+		$(".prime_way").show();
+		$(".bg_color").show();
+	},function(){
+		$(".prime_way").hide();
+		$(".bg_color").hide();
+	})
+	
+	$(".lan_bar,.change_lan").hover(function(){
+		$(".change_lan").show();
+		$(".bg_color").show();
+	},function(){
+		$(".change_lan").hide();
+		$(".bg_color").hide();
+	})
+	
+	$(".now_price").click(function(){
+		$(this).css("border","solid 1px dodgerblue");
+	}).blur(function(){
+		$(this).css("border","solid 1px transparent");
+	})
+	
+	//倒计时
+	downTimeFun();
 })
